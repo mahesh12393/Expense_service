92a7a692742db1a9a363ed461ca376362da3b434
Pulla Maheswar Reddy <maheswar.reddy.pulla@sap.com>
2025-07-12 17:40:14 +0530

    exposed add service endpoint for testing

--- src/main/java/com/expense/service/controller/ExpenseController.java
+++ src/main/java/com/expense/service/controller/ExpenseController.java
@@ -7,8 +7,7 @@ import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +30,21 @@ public class ExpenseController {
             ex.printStackTrace();
             return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PostMapping(path="/addExpense")
+    public ResponseEntity<Boolean> addExpenses(@RequestHeader(value = "X-User-Id") @NonNull String userId, @RequestBody ExpenseDto expenseDto){
+        try{
+            expenseDto.setUserId(userId);
+            return new ResponseEntity<>(expenseService.createExpense(expenseDto), HttpStatus.OK);
+        }catch (Exception ex){
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<Boolean> checkHealth(){
+        return new ResponseEntity<>(true, HttpStatus.OK);
     }
 
 
